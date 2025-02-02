521f4baad57818730c2e3169871e1433269c1da0
Guillermo <gsolorzanoo@est.utn.ac.cr>
2025-02-02 08:25:11 -0600

    Workshop2 Terminado

---
+++ Workshop2/server/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+//middlewares
+app.use(bodyParser.json());
+app.use(cors({
+  domains: '*',
+  methods: '*'
+}));
+
+//routes
+app.get('/tipocambio', function (req, res) {
+  let response = {};
+  switch(req.query.type) {
+    case 'usd':
+      response = {
+        "TipoCompraDolares" : "621",
+        "TipoVentaDolares" : "621"
+      }
+    break;
+    case 'eur':
+      response = {
+        "TipoCompraEuros" : "731.85",
+        "TipoVentaEuros" : "761.9"
+      }
+    break;
+    default:
+      response = {
+        "TipoCompraDolares" : "621",
+        "TipoVentaDolares" : "621",
+        "TipoCompraEuros" : "731.85",
+        "TipoVentaEuros" : "761.9"
+      }
+    break;
+  }
+  res.json(response);
+});
+
+//start the app
+app.listen(3001, () => console.log(`BBCR Exchange type service listening on port 3001!`))
